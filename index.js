6eefaad5e23f84d87ff94f4735fb36dc40fabb24
yesI <1317154549@qq.com>
2017-09-19 03:15:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var itemPrice = Math.random()*100;
+  var itemPrice = Math.floor(Math.random()*100);
   var it = {item: itemPrice};
  // write your code here
  cart.push(it);
